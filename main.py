1eecde66f8d5d9e887d94fd07acbace6c55006cf
Eduardo Flores <eduardo.floresramones@gmail.com>
2023-03-04 17:54:06 -0600

    greet the user

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 # Bot library
+import logging
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
 # Get the Telegram API token
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
-async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+# Greet user
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text='¡Hola! Puedes reportar espacios seguros o peligrosos por medio de este bot.')
 
+# Main process
+if __name__ == '__main__':
 
-app = ApplicationBuilder().token(os.getenv('TOKEN')).build()
+    # Start and set up the bot
+    application = ApplicationBuilder().token(os.getenv('TOKEN')).build()
+    
+    # First time use, greet the user
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
 
-app.add_handler(CommandHandler("hello", hello))
-
-app.run_polling()
+    # Keep the bot listening for user input
+    application.run_polling()
